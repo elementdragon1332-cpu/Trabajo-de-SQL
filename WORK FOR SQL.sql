f720c90cd4e1939f8c61ef77c8b725a4755440a9
elementdragon1332-cpu <element.dragon1332@gmail.com>
2026-03-17 21:43:43 -0300

    Add SQL commands for database creation
    Added SQL commands to create and use the DESAFIO database.

--- WORK FOR SQL.sql
+++ WORK FOR SQL.sql
@@ -1,5 +1,3 @@
-Esse foi feito por mim e pela IA
-
 -- 1. Criar o Schema (se já existir, pode dar erro - nesse caso pule essa linha ou use DROP DATABASE IF EXISTS DESAFIO;)
 CREATE DATABASE DESAFIO;
 USE DESAFIO;
